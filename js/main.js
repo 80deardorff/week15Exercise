5306ba227d1aba39a791d977fa82b93619039e5d
Adrian Deardorff <80deardorff@live.com>
2018-08-14 22:48:32 -0700

    Finished exercise.

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var todo = new Vue({
+    el:'#todo',
+    data: {
+        newTask:'',
+        todoList:[]
+    },
+    methods: {
+        addTask: function() {
+            var task = this.newTask;
+            this.todoList.push(task);
+            this.newTask = '';
+        },
+        removeTask: function(task) {
+            var index = this.todoList.indexOf(task);
+            this.todoList.splice(index, 1);
+        },
+        removeAllTasks: function() {
+            this.todoList = [];
+        }
+        // editTask: function(task) {
+        //     var index = this.todoList.indexOf(task);
+        // }
+    }
+});
